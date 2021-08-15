de7d5a81c1eb9af1bf09ee56aaff7056535e3b40
onioneffect <admin@explosive.love>
2021-08-14 21:42:56 -0400

    Adds functions to decode images from argv.

--- champion.go
+++ champion.go
@@ -2,20 +2,61 @@ package main
 
 import (
 	"fmt"
-	//"image/png"
+	"image"
+	_ "image/png"
 	"os"
 )
 
-func main() {
-	fmt.Println("Hello!")
+type ImageInfo struct {
+	Format        string
+	Width, Height int
+	Data          *image.Image
+}
 
-	for i := 0; i < len(os.Args); i++ {
-		imgFile, err := os.Open(os.Args[i])
+func read_img_info(img_reader *os.File) ImageInfo {
+	var return_info ImageInfo
+
+	config, format, err := image.DecodeConfig(img_reader)
+	if err != nil {
+		panic(err)
+	}
+	return_info.Height = config.Height
+	return_info.Width = config.Width
+	return_info.Format = format
+
+	_, err = img_reader.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	img_data, _, err := image.Decode(img_reader)
+	if err != nil {
+		panic(err)
+	}
+	return_info.Data = &img_data
+
+	return return_info
+}
+
+func print_img_info(imginf ImageInfo) {
+	fmt.Printf("Image dimensions: %d, %d\n", imginf.Width, imginf.Height)
+	fmt.Println("Image format:", imginf.Format)
+	fmt.Println("First pixel:", (*imginf.Data).At(0, 0))
+}
+
+func img_processor(fp *os.File) {
+	var current_img ImageInfo = read_img_info(fp)
+	print_img_info(current_img)
+}
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		img_file, err := os.Open(os.Args[i])
 		if err != nil {
 			panic(err)
 		}
-		defer imgFile.Close()
-	}
 
-	return
+		img_processor(img_file)
+		img_file.Close()
+	}
 }
