eeaf3a3b71bdf47810a69148c516bca232512f68
onioneffect <admin@explosive.love>
2021-08-17 23:39:21 -0400

    Adds a function to iterate through a picture. Taken str8 from SO.

--- champion.go
+++ champion.go
@@ -7,13 +7,36 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"os"
+
+	"golang.org/x/image/draw"
 )
 
 type ImageInfo struct {
+	Bounds        image.Rectangle
 	Format        string
 	ColorModel    color.Model
 	Width, Height int
 	Data          *image.Image
+}
+
+// Thanks to https://stackoverflow.com/questions/33186783/
+func image_array(im ImageInfo) [][][3]float32 {
+	width, height := im.Width, im.Height
+	iaa := make([][][3]float32, height)
+	src_rgba := image.NewRGBA(im.Bounds)
+	draw.Copy(src_rgba, image.Point{}, (*im.Data), im.Bounds, draw.Src, nil)
+
+	for y := 0; y < height; y++ {
+		row := make([][3]float32, width)
+		for x := 0; x < width; x++ {
+			idx_s := (y*width + x) * 4
+			pix := src_rgba.Pix[idx_s : idx_s+4]
+			row[x] = [3]float32{float32(pix[0]), float32(pix[1]), float32(pix[2])}
+		}
+		iaa[y] = row
+	}
+
+	return iaa
 }
 
 func read_img_info(img_reader *os.File) ImageInfo {
@@ -38,6 +61,7 @@ func read_img_info(img_reader *os.File) ImageInfo {
 	if err != nil {
 		panic(err)
 	}
+	return_info.Bounds = img_data.Bounds()
 	return_info.Data = &img_data
 
 	return return_info
@@ -52,12 +76,15 @@ func (imginf ImageInfo) print_img_info() {
 	// Thanks to https://stackoverflow.com/questions/45226991/
 	fmt.Printf("Image color model: %T\n", imginf.ColorModel.Convert(color.RGBA{}))
 
+	fmt.Println("Image bounds:", (*imginf.Data).Bounds())
 	fmt.Println("First pixel:", (*imginf.Data).At(0, 0))
 }
 
 func img_processor(fp *os.File) {
 	var currentImg ImageInfo = read_img_info(fp)
+	var asdasd [][][3]float32 = image_array(currentImg)
 	currentImg.print_img_info()
+	fmt.Println(">>>", asdasd[0][0])
 }
 
 func main() {
