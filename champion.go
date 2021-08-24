6fe8a3190f42627d2d777bd9b0d83896c0b021c4
onioneffect <admin@explosive.love>
2021-08-23 23:10:26 -0400

    Adds image_array_gray

--- champion.go
+++ champion.go
@@ -29,6 +29,26 @@ type Line struct {
 	Start, End [2]int32
 }
 
+func image_array_gray(im ImageInfo) [][]int32 {
+	width, height := im.Width, im.Height
+	iaa := make([][]int32, height)
+	src_rgba := image.NewRGBA(im.Bounds)
+	draw.Copy(src_rgba, image.Point{}, (*im.Data), im.Bounds, draw.Src, nil)
+
+	for y := 0; y < height; y++ {
+		row := make([]int32, width)
+		for x := 0; x < width; x++ {
+			idx_s := (y*width + x) * 4
+			pix := src_rgba.Pix[idx_s : idx_s+4]
+			row[x] = int32(pix[0])
+		}
+
+		iaa[y] = row
+	}
+
+	return iaa
+}
+
 // Thanks to https://stackoverflow.com/questions/33186783/
 func image_array(im ImageInfo) [][][3]int32 {
 	width, height := im.Width, im.Height
