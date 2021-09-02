aaed8418d86f81339586230ac765957e6d36a7ba
onioneffect <admin@explosive.love>
2021-09-01 21:19:06 -0400

    Adds RGBToHex function

--- lib/line.go
+++ lib/line.go
@@ -4,11 +4,27 @@ package imagelib
 import "fmt"
 
 type Line struct {
-	// Try to store the 24-bit color representation in the lower bits of int32.
-	HexColor int32
+	// HexColor is just an array with the RGB values.
+	HexColor [3]int32
 
 	// Start and end are represented by x and y, so four numbers in total.
 	Start, End [2]int32
+}
+
+func (l Line) RGBToHex() (string, error) {
+	var fmtString string
+	var retString string = "#"
+
+	for _, i := range l.HexColor {
+		fmtString = "%X"
+		if i < 16 {
+			fmtString = "0" + fmtString
+		}
+
+		retString += fmt.Sprintf(fmtString, uint8(i))
+	}
+
+	return retString, nil
 }
 
 func ImagePixLoop(im ImageInfo) {
